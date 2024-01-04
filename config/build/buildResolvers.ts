2a24c3c598e136b6f53cdc217f1607f390a136e6
ignatiygotovchikov <ignatsergeevich10@gmail.com>
2024-01-04 10:26:08 +0200

    added file structure and AppRouter config

--- config/build/buildResolvers.ts
+++ config/build/buildResolvers.ts
@@ -1,7 +1,12 @@
-import {ResolveOptions} from "webpack";
+import { ResolveOptions } from "webpack";
+import { BuildOptions } from "./types/config";
 
-export function buildResolvers(): ResolveOptions {
-    return {
-        extensions: ['.tsx', '.ts', '.js'],
-    }
+export function buildResolvers(opitions: BuildOptions): ResolveOptions {
+  return {
+    extensions: [".tsx", ".ts", ".js"],
+    preferAbsolute: true,
+    modules: [opitions.paths.src, "node_modules"],
+    mainFiles: ["index"],
+    alias: {},
+  };
 }
