d56053ba87d4387efd87aafaf21479c7e8c993fa
ignatiygotovchikov <ignatsergeevich10@gmail.com>
2024-01-08 18:24:04 +0200

    added babel extract plugin

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -43,5 +43,25 @@ export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
     ],
   };
 
-  return [svgLoader, typescriptLoader, cssLoader, fileLoader];
+  const babelLoader = {
+    test: /\.(js|jsx|tsx)$/,
+    exclude: /node_modules/,
+    use: {
+      loader: "babel-loader",
+      options: {
+        presets: ["@babel/preset-env"],
+        plugins: [
+          [
+            "i18next-extract",
+            {
+              locales: ["ru", "en"],
+              keyAsDefaultValue: true,
+            },
+          ],
+        ],
+      },
+    },
+  };
+
+  return [fileLoader, svgLoader, babelLoader, typescriptLoader, cssLoader];
 }
