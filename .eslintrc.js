ba0ff4e5eee3d682523e60cff6b9d87f7e9fb8ba
ignatiygotovchikov <ignatsergeevich10@gmail.com>
2024-01-26 22:15:07 +0200

    some changes for i18n

--- .eslintrc.js
+++ .eslintrc.js
@@ -39,11 +39,25 @@ module.exports = {
         "import/no-extraneous-dependencies": "off",
         "no-underscore-dangle": "off",
         "object-curly-spacing": ["error", "always"],
-        "i18next/no-literal-string": ["error", { markupOnly: true }],
+        "i18next/no-literal-string": [
+            "error",
+            {
+                markupOnly: true,
+                ignoreAttribute: ["data-testid"],
+            },
+        ],
         "max-len": ["error", { "ignoreComments": true, code: 100 }],
         quotes: ["error", "double"],
     },
     globals: {
         __IS_DEV__: true,
     },
+    overrides: [
+        {
+            files: ["**/src/**/*.test.{ts,tsx}"],
+            rules: {
+                "i18next/no-literal-string": "off",
+            },
+        },
+    ],
 };
