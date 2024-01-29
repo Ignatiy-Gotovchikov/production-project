32975da134d653387369c022832a4f4fe52b996e
ignatiygotovchikov <ignatsergeevich10@gmail.com>
2024-01-29 19:06:13 +0200

    added modal, portal

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,18 +1,25 @@
-import { Suspense } from "react";
+import { Suspense, useState } from "react";
 import "./styles/index.scss";
 import { classNames } from "shared/lib/classNames/classNames";
 import { useTheme } from "app/providers/ThemeProvider";
 import { Sidebar } from "widgets/Sidebar";
 import { Navbar } from "widgets/Navbar";
+import Modal  from "shared/ui/Modal/Modal";
 import { AppRouter } from "./providers/routers";
 
 const App = () => {
     const { theme } = useTheme();
 
+    const [isOpen, setIsOpen] = useState(false)
+
     return (
         <div className={classNames("app", {}, [theme])}>
             <Suspense fallback="">
                 <Navbar />
+                <button onClick={()=> setIsOpen(true)}>toggle</button>
+                <Modal isOpen={isOpen} onClose={()=>setIsOpen(false)}>
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laborum fuga mollitia perferendis ab maxime praesentium recusandae quidem aliquam doloribus quam, autem saepe expedita incidunt laudantium eveniet, itaque consequatur. Laborum, distinctio?
+                </Modal>
                 <div className="content-page">
                     <Sidebar />
                     <AppRouter />
